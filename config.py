f53168d964554c77320499fd4544f7dd07124088
Muhammad Umair <umairbwp202@gmail.com>
2025-04-30 02:49:12 +0500

    test

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
-import os
-
-DB_NAME = os.getenv("DB_NAME", "demo")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASS = os.getenv("DB_PASS", "admin")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-SECRET_KEY = os.getenv("SECRET_KEY", "SECRET_KEY")
-
-# Email config
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.getenv("umairbwp@gmail.com")
-MAIL_PASSWORD = os.getenv("admin")
+DB_NAME = "demo"
+DB_USER = "postgres"
+DB_PASS = "admin"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+SECRET_KEY = 'secret_key'
